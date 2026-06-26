e217e19f886894d8a622fd01ba79356c5799f537
Seokseong Jeon <sjeon87@gmail.com>
2026-06-26 14:54:41 +0900

    Add Win11 fast setup PowerShell script

---
+++ win11-setup.ps1
@@ -0,0 +1,37 @@
+################################################################################
+# Pre-requisites                                                               #
+################################################################################
+# Run `Set-ExecutionPolicy Unrestricted` as Administrator
+
+function InstallWingetPkg {
+  param (
+    [Parameter(Mandatory=$true)]
+    [string]$appId,
+    [Parameter(Mandatory=$false)]
+    [string]$appName
+  )
+  if (-not $appName) {
+    $appName = $appId
+  }
+
+  $null = winget list --id $appId --exact 2>$null
+  if ($LASTEXITCODE -eq 0) {
+    Write-Host "$appName is already installed."
+  } else {
+    Write-Host "Installing $appName..."
+    winget install $appId --source winget --silent --accept-source-agreements --accept-package-agreements
+    if ($LASTEXITCODE -eq 0) {
+      Write-Host "$appName is successfully installed."
+    } else {
+      Write-Host "Encountered an error during $appName installation. Error code: $LASTEXITCODE."
+    }
+  }
+}
+
+InstallWingetPkg("Git.Git")
+InstallWingetPkg("Microsoft.PowerToys")
+InstallWingetPkg("voidtools.Everything")
+
+Write-Host "`nPress any key to close:"
+$null = [System.Console]::ReadKey($true)
+
