ffe38ea6eeba80567cff78068bac22cfc322abc5
Seokseong Jeon <sjeon87@gmail.com>
2026-06-26 16:14:10 +0900

    Adjust desktop system icons with win11 fast setup script

--- win11-setup.ps1
+++ win11-setup.ps1
@@ -32,6 +32,25 @@ function InstallWingetPkg {
   }
 }
 
+################################################################################
+# Desktop system icons                                                         #
+################################################################################
+# Document (Home folder)
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" -Name "{59031a47-3f72-44a7-89c5-5595fe6b30ee}" -Value 1
+# Computer
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" -Name "{20D04FE0-3AEA-1069-A2D8-08002B30309D}" -Value 1
+# Trash bin
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" -Name "{645FF040-5081-101B-9F08-00AA002F954E}" -Value 0
+# Network
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" -Name "{F02C1A0D-BE21-4350-88B0-7367FC96EF3C}" -Value 1
+# Control panel
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" -Name "{53994ECE-340E-4B0C-901E-44E845144A28}" -Value 1
+# Restart Explorer for immediate update
+Stop-Process -Name explorer -Force
+
+################################################################################
+# Install softwares                                                            #
+################################################################################
 InstallWingetPkg("Git.Git")
 InstallWingetPkg("Microsoft.PowerToys")
 InstallWingetPkg("voidtools.Everything")
